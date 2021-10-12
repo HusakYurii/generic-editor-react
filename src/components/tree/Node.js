23fbaae266a1ea64ca0ce97ed7408c4a69c490b3
Husak Yurii <gusak.1881@gmail.com>
2021-10-12 21:33:37 +0200

    add diferent colors to test

--- src/components/tree/Node.js
+++ src/components/tree/Node.js
@@ -3,7 +3,8 @@ import "./node.css";
 
 export const Node = ({ node }) => {
     const nodes = node.nodes.map(node => <Node key={node.id} node={node} />);
-    const toggler = node.nodes.length > 0 ? <span className="toggler">x</span> : null;
+
+    const toggler = node.nodes.length > 0 ? <span className="toggler">+</span> : null;
 
     return (
         <div className="node">
