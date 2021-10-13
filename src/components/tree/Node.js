e4fb6431b7acf90f8061348466494a517771ed63
Husak Yurii <gusak.1881@gmail.com>
2021-10-13 20:30:55 +0200

    add toggling functionality

--- src/components/tree/Node.js
+++ src/components/tree/Node.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./node.css";
 
 export const Node = ({ node }) => {
-    const nodes = node.nodes.map(node => <Node key={node.id} node={node} />);
+    const [collapsed, setCollapsed] = useState(true);
 
-    const toggler = node.nodes.length > 0 ? <span className="toggler">+</span> : null;
+    const nodesList = <div className="node-nodes">{node.nodes.map(node => <Node key={node.id} node={node} />)}</div>;
+
+    const toggler = <span className="toggler" onClick={() => setCollapsed(!collapsed)}>{collapsed ? "+" : "-"}</span>;
 
     return (
         <div className="node">
             <div className="node-indicator">
-                {toggler}
+                {node.nodes.length === 0 ? null : toggler}
                 <span className="name">{node.name}</span>
             </div>
-            <div className="node-nodes">{nodes}</div>
+            {collapsed ? null : nodesList}
         </div>
     );
 }
