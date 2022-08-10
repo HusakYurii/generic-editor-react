16aadd58788823b360a6cee7772400188a0229ee
Husak Yurii <gusak.1881@gmail.com>
2022-08-10 20:54:28 +0200

    modify tools to modify data in redux fasion

--- src/tools/uidGenerator.js
+++ src/tools/uidGenerator.js
@@ -1,10 +1,4 @@
-/**
- * Use this function fo debugging 
- */
-// @TODO replace it with a function that provides random UIDs
-export const createGenerator = function (startFrom = 0) {
-    let uid = startFrom;
-    return function () {
-        return (uid++);
-    }
+
+export const getUID = function () {
+    return Math.ceil(Math.random() * 10000);
 }
