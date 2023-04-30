6214ba4b8e2235d2817f9d537660e342f907d0b1
Husak Yurii <gusak.1881@gmail.com>
2023-04-30 22:12:45 +0200

    add meta data to exported files and format the code

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import store from "../../store";
 import { convertFileToBase64, exportJSONFile } from "../../tools/resourcesTools";
+import { VERSION } from "../../VERSION";
 
 /**
  * 
@@ -25,29 +26,64 @@ const convertResourcesRecursively = (queue, memo, onFinish) => {
 }
 
 
+/**
+ * To save resources which are used and export them in the map in base 64 format
+ * @param {import("../../store").IStore} store 
+ */
+const exportResourcesAsBase64 = (store) => {
+    const { resourcesList, spritePropertiesList } = store.getState();
+
+    const resourcesToExport = Object.values(spritePropertiesList).reduce((acc, properties) => {
+        if (properties.resourceID) {
+            acc.push([properties.resourceID, resourcesList[properties.resourceID]])
+        }
+        return acc;
+    }, []);
+
+    convertResourcesRecursively(resourcesToExport, {}, (resources) => {
+        exportJSONFile(
+            JSON.stringify({
+                meta: {
+                    version: VERSION,
+                    type: "data-resources-base64",
+                    description: "This file contains the resources. The resources are in base64 format"
+                },
+                resources
+            }, null, 2),
+            "assesMapAsBase64.json"
+        );
+    });
+};
+
+/**
+ * To make a bundle with all the data and save it as json file
+ * @param {import("../../store").IStore} store 
+ */
+const exportMainData = (store) => {
+    const { tree, entityTypesList, basePropertiesList, spritePropertiesList } = store.getState();
+
+    exportJSONFile(
+        JSON.stringify({
+            meta: {
+                version: VERSION,
+                type: "data-main",
+                description: "This file is a bundle and contains the tree hierarchy, entity types each node represents, base and sprite properties"
+            },
+            treeData: tree.treeData,
+            entityTypesList,
+            basePropertiesList,
+            spritePropertiesList
+        }, null, 2),
+        "mainData.json"
+    );
+};
+
+
 export const Header = () => {
 
     const exportData = () => {
-        const { tree, entityTypesList, resourcesList, basePropertiesList, spritePropertiesList } = store.getState();
-
-        exportJSONFile(
-            JSON.stringify({ treeData: tree.treeData, entityTypesList, basePropertiesList, spritePropertiesList }, null, 2),
-            "mainData.json"
-        );
-
-        const resourcesToExport = Object.values(spritePropertiesList).reduce((acc, properties) => {
-            if (properties.resourceID) {
-                acc.push([properties.resourceID, resourcesList[properties.resourceID]])
-            }
-            return acc;
-        }, [])
-
-        convertResourcesRecursively(resourcesToExport, {}, (resources) => {
-            exportJSONFile(
-                JSON.stringify(resources, null, 2),
-                "assesMapAsBase64.json"
-            );
-        });
+        exportMainData(store);
+        exportResourcesAsBase64(store);
     };
 
     return (
